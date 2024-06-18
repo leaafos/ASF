6f0e36af19a45402d64e544caac1ef758e60820e
leaafos <leafostacz@gmail.com>
2024-06-18 21:47:50 +0200

    ajout modifications

--- src/pages/sports/Aikido.jsx
+++ src/pages/sports/Aikido.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../styles/sports.css";
 
 const Aikido = () => {
+    const [showOthers, setShowOthers] = useState(false);
+
+    const handleToggle = () => {
+     setShowOthers(!showOthers);
+  };
   return (
     <div>
         <div className='header aikido'>
@@ -60,13 +65,40 @@ const Aikido = () => {
             </div>
             <h2>INSCRIPTIONS</h2>
             <hr className='ligne-titre'></hr>
-            <div className='inscription'>
-                <h4>INSCRIPTION POSSIBLE VIA LE LIEN SUIVANT :</h4>
-                <span className='lien'>
-                    <a href="https://asfontenaisienne.monclub.app/">
-                       LIEN D'INSCRIPTION 
-                    </a>
-                </span>
+            <div className='bloc'>
+                <h4 className='h4'>GUIDE D'INSCRIPTION À DESTINATION DE NOS ADHÉRENT(E)S :</h4>
+                <div>
+                    <span className='lien'>
+                        <a href="">
+                        PARCOURS D'INSCRIPTION SUR LE WEB (NOTION.SITE) 
+                        </a>
+                    </span>
+                    <p>Il s'agit d'une page du site internet de MonClub qui explique aux adhérents comment s'inscrire pour la première fois avec MonClub ou bien comment se connecter si un compte a déjà été créé.</p>
+                </div>  
+            </div>
+            <div className='bloc'>
+                <h4 className='h4'>DISPOSITIF CERTIFICAT MÉDICAL POUR LE SPORT - INFORMATIONS ADHÉRENT</h4>
+                <div>
+                    <span className='lien'>
+                        <a href="">
+                        CLIQUEZ ICI
+                        </a>
+                    </span>
+                    <h5>Mineur : </h5>
+                    <p>Questionnaire de santé (à garder par l'adhérent) ou Certificat Médical (si + de 3 ans) si nouveau ou souhait de la section. </p>
+                    <h5>Majeur : </h5>
+                    <p>Questionnaire de santé (à garder par l'adhérent) ou Certificat Médical (si + de 3 ans) si nouveau ou souhait de la section. </p>
+                </div>
+            </div>
+            <div className='bloc'>
+                <h4 className='h4'>INSCRIPTION EN LIGNE :</h4>
+                <div>
+                    <span className='lien'>
+                        <a href="https://asfontenaisienne.monclub.app/">
+                        LIEN D'INSCRIPTION 
+                        </a>
+                     </span>
+                </div>
             </div>
             <h2>COTISATIONS</h2>
             <hr className='ligne-titre'></hr>
@@ -86,6 +118,63 @@ const Aikido = () => {
                 <div className='case-cotisation'>
                     <p><b>ARMES</b></p>
                     <p className='prix-cotisation'><b>215€</b></p>
+                </div>
+            </div>
+            <div className='paiement'>
+                <h3>MOYENS DE PAIEMENT</h3>
+                <div>
+                    <ul>
+                        <li>
+                            <h4>CARTE BANCAIRE</h4>
+                            <p>(en une seule fois)</p>
+                        </li>
+                        <li>
+                            <h4>SEPA (PRÉLÈVEMENTS)</h4>
+                            <p>(en 3 fois)</p>
+                        </li>
+                        <li>
+                            <h4 onClick={handleToggle} className='menu-deroulant'>
+                                AUTRES MOYENS
+                                <span>
+                                    {showOthers ? 'v' : '>'}
+                                </span>
+                                </h4>
+                            {showOthers && (
+                                <div className='autres-moyens'>
+                                    <h4>Si Bénéficiaire d'Aides :</h4>
+                                    <p>ANVC (coupons sport, chèque-vacances), Coup de pouce loisirs (CAF), Pass+, Pass'sport, Chèque sport, Labaz, Coupon Sceaux</p>
+                                    <p><i>(Uniquement : Athlétisme, Badminton (en compétition), Gymnastique Rythmique, Gymnastique Artistique, Handball, Rugby, Squash, VolleyBall)</i></p>
+                                    
+                                    <h4>Par chèque(s) :</h4>
+                                    <p>(5 fois maximum - à l'ordre de l'ASF) </p>
+                                    
+                                    <h4>Par espèces : </h4>
+                                    <p>(en 1 seule fois)</p>
+                                    <div className='remise'>
+                                        <h4>La remise se fera : </h4>
+                                    </div>
+                                    <p>Au Forum des Associations le <b className='gras'> 08 septembre</b> au Gymnase du Parc entre 10h00 et 18h00
+                                    </p>
+                                    <p>OU</p>
+                                    <p>Auprès de l'Accueil / Secrétariat de l'ASF - 10 place du château Ste Barbe - 2ème Étage - Porte 7</p>
+                                    
+                                    <h4>Par virement : </h4>
+                                    <p>(en 1 seule fois)</p>
+                                    <ul>
+                                        <li>Se rapprocher de la section pour obtenir le RIB</li>
+                                        <li>Se connecter à l'application de votre banque en ligne - Sélectionner virement</li>
+                                        <li>Ajouter un nouveau bénéficiaire</li>
+                                        <li>Indiquer <b className='gras'>obligatoirement</b> le motif du virement</li>
+                                        <ul>
+                                            <li>a. Section concernée + Nom et Prénom de l'enfant si inscription d'un mineur ou de l'adulte inscrit </li>
+                                            <li>b. Exemple : ASF VOLLEY - enfant AZEERTYUI Noémie ou ASF VOLLEY - adulte AZERTYUI Marc</li>
+                                        </ul>
+                                        <li>Effectuer le virement</li>
+                                    </ul>
+                                </div>
+                            )}
+                        </li>
+                    </ul>
                 </div>
             </div>
             <h2>CONTACTS / RENSEIGNEMENTS</h2>
